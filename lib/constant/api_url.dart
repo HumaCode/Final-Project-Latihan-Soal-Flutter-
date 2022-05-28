035742ded6923c76463057a39765b73ce3b0de48
humaidi <humaidizakaria1998@gmail.com>
2022-05-28 13:11:01 +0700

    API base Url dan Endpoint

---
+++ lib/constant/api_url.dart
@@ -0,0 +1,16 @@
+class ApiUrl {
+  // definisikan base url
+  static String baseUrl = "https://ekskul.id/api/";
+
+  // api user
+  static String users = "users";
+  static String userRegistrasi = "users/registrasi";
+  static String userUpdateProfile = "users/update";
+
+  // api latihan
+  static String latihanMapel = "exercise/data_course";
+  static String latihanPaketSoal = "";
+  static String latihanKerjakanSoal = "exercise/kerjakan";
+  static String latihanSubmitJawaban = "exercise/input_jawaban";
+  static String latihanSkor = "exercise/score_result";
+}
